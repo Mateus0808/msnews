d7b986ded597e134648355188ff05caa697913c6
Mateus0808 <loiolamateus7@gmail.com>
2021-12-18 23:59:08 -0300

    sign up for stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,41 @@
-import { FaGithub } from "react-icons/fa";
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-browser";
 
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
-} 
+}
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe?.redirectToCheckout({ sessionId });
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      onClick={handleSubscribe}
+      className={styles.subscribeButton}
+    >
       Subscribe Now
     </button>
   );
